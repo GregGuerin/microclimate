c77ed437deeee2c14a9737f82d7252f601a9f132
Greg Guerin <GregGuerin@users.noreply.github.com>
2022-05-03 15:27:15 +0930

    Update microclimate_code_final_INTERACTIVE.R

--- R/microclimate_code_final_INTERACTIVE.R
+++ R/microclimate_code_final_INTERACTIVE.R
@@ -1,9 +1,8 @@
 ###############################################################
 #Step 1 - compile data from all sensors into one object and load sun times data
-template <- read_sensor_data("/KA/KA*")
+my.sensor.data <- read_sensor_data("/KA/KA*")
 
-#the 'template' object returned above should have all the data from the .txt files in the folder together in one data frame with the 'Sensor' field denoting which file/sensor it came from.
-
+#the 'my.sensor.data' object returned above should have all the data from the .txt files in the folder together in one data frame with the 'Sensor' field denoting which file/sensor it came from.
 
 ##################################
 
@@ -16,11 +15,11 @@ rise_set <- read.csv("rise_set_times.csv") #e.g.
 ###############################################################
 #Step 2 - process the raw data to include sun times, code as day or night, add season etc
 
-template <- format_sensor_data(template, rise_set) 
+my.sensor.data <- format_sensor_data(my.sensor.data, rise_set) 
 
 ###############################################################
 #Step 3 - iterate through sensor and day to get daily climate values for min / max / mean / standard deviation / range and then calculate mean values per night/day for month, season and 'year' (year being all the data)
 
-temp_means <- calculate_means(template) #returns night/day mean max/mean/min/range/sd/CV temps for winter & summer
+temp_means <- calculate_means(my.sensor.data) #returns night/day mean max/mean/min/range/sd/CV temps for winter & summer
 
 #################
